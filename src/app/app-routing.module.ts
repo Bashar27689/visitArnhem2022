87b229b7c5faf2e8dc5e27275d9e950512da1682
Bashar Mohamad <bashar.mohamad.bee@outlook.com>
2022-03-11 01:31:37 +0100

    b

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HotelsComponent } from './hotels/hotels.component';
+import { MilestonesComponent } from './milestones/milestones.component';
+import { MuseumsComponent } from './museums/museums.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/Visitarnhem', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'milestones', component: MilestonesComponent },
+  { path: 'museums', component: MuseumsComponent },
+  { path : 'restaurants',component:RestaurantsComponent},
+  {path:'hotels',component: HotelsComponent},
+  {path:'contactus',component:ContactusComponent}
+ 
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
